8a656a96b647547f430642a3edc74bac5ef4ac0f
AndreasParperis23 <79907508+AndreasParperis23@users.noreply.github.com>
2022-03-17 22:06:33 +0200

    more changes

--- script.js
+++ script.js
@@ -47,6 +47,83 @@ let list_url='https://codecyprus.org/th/api/list';
 
 getList();
 
+const id_params = new URLSearchParams(location.search);
+const treasureHuntID = id_params.get("treasureHuntID");
+
+console.log(treasureHuntID);
+
+
+function start()
+{
+    const playerName = document.getElementById("playerName").value;
+    const appName = "Team5App";
+    const START_URL = "https://codecyprus.org/th/api/start?player=" + playerName + "&app=" + appName + "&treasure-hunt-id=" + treasureHuntID;
+
+    console.log(START_URL);
+
+    fetch(START_URL)
+        .then(response=> {
+            return response.json();
+        })
+        .then(data=>{
+
+            console.log(data);
+            if(data.status=== "ERROR") {
+                alert(data.errorMessages);
+            }
+            else
+            {
+                window.location.href="startGame.html";
+                console.log(session);
+
+            }
+
+        })
+
+}
+
+start();
+
+const session_params = new URLSearchParams(location.search);
+const session=session_params.get("session");
+console.log(session);
+
+
+
+async function getQuestions()
+{
+    const QUESTIONS_URL="https://codecyprus.org/th/api/question?session" + session;
+
+    const res= await fetch(QUESTIONS_URL);
+    const data= await res.json();
+
+
+    console.log(data);
+
+    if(data.status === "OK" && session === session_params.get("session"))
+    {
+
+        for(let q=0; q < data.numOfQuestions-1; q++)
+        {
+            let question=document.createElement("h2");
+            question.textContent=data.questionText;
+            //question.href=QUESTIONS_URL;
+            let q_div=document.getElementById('question');
+            q_div.append(question);
+            q_div.append(document.createElement("br"));
+
+        }
+
+
+    }
+    else
+    {
+        alert(data.errorMessages);
+    }
+
+
+}
+getQuestions();
 
 
 
